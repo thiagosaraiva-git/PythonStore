b5389e54eaacc4bcd132918640d82e53dd59fefc
Thiago Saraiva <saraii_@live.com>
2022-07-14 18:42:10 -0300

    Update start.py

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ cart =[]
 def menu():
     option = 0 #variável que armazena a opção escolhida pelo usuário
 
-    while option != "4": #enquanto a opção escolhida não for 4, o programa continua
+    while True: #enquanto a opção escolhida não for 4, o programa continua
         cls()
         banner() #imprime o banner do programa
         option = input("\n1 - Adicionar um produto ao carrinho \n2 - Remover um produto do carrinho \n3 - Visualizar carrinho \n4 - Finalizar compra\nDigite a opção desejada: ")
